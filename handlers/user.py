c788d82e818d053d83bdf86e29adbbbb3f2fcb09
Craig Dsouza <craigds022@gmail.com>
2025-07-29 21:21:04 +0530

    forcing new custom domain for mini app

--- handlers/user.py
+++ handlers/user.py
@@ -120,7 +120,7 @@ async def app(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     user_str = f"User {user.id} ({user.first_name} {user.last_name})"
     logger.info(f"[APP] {user_str} - /app command received")
-    mini_app_url = os.environ.get("MINI_APP_URL", "https://telegram-mini-app-production-8aae.up.railway.app/")
+    mini_app_url = os.environ.get("MINI_APP_URL") # "https://telegram-mini-app-production-8aae.up.railway.app/"
 
     # Create a keyboard with a Web App button
     keyboard = [
